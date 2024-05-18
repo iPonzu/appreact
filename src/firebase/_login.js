078644421184f7164ad9ce571a80ff3bf981ee02
Your Name <you@example.com>
2024-05-17 21:27:46 -0300

    adding firebase config

---
+++ src/firebase/_login.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEeK0xz3A6bm30CuKamSVnYeC_R3c-wdo",
+  authDomain: "react-app-e92bc.firebaseapp.com",
+  projectId: "react-app-e92bc",
+  storageBucket: "react-app-e92bc.appspot.com",
+  messagingSenderId: "561852770792",
+  appId: "1:561852770792:web:a660ea56786583d18a5c5d"
+};
+
+const app = initializeApp(firebaseConfig);
