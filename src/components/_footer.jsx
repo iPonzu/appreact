0bf491f03c75538e6feffc6cc1fae9550993d43c
Your Name <you@example.com>
2024-06-04 20:59:53 -0300

    adding footer component

---
+++ src/components/_footer.jsx
@@ -0,0 +1,10 @@
+
+export default function _footer(){
+    return(
+        <footer>
+            <nav className="nav justify-content-center items-end">
+                <code className="text-white fs-10">Desenvolvido com React, Bootstrap e login com Firebase</code>
+            </nav>
+        </footer>
+    )
+}
