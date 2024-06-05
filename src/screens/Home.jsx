5e726f7918cc8e48e2e48ced36610d4c041b36f3
Your Name <you@example.com>
2024-06-04 21:01:41 -0300

    adding state

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,21 @@
 import _header from '../components/_header'
+import React, { useState } from 'react'
 
 export default function Home(){
+    const [value, setValue] = useState('')
+    const [tax, setTax] = useState('')
+
+    const handleCalculate = () => {
+        const taxValue = value * 0.6
+        setTax(taxValue)
+    }
+
     return(
         <>
             <_header />
+            <div>
+                {/*TODO>> start tax calculator */}
+            </div>
             
         </>
     )
